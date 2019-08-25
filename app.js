85586d9dda59079a25b729dffd8fe9a6be41494e
woodenrabbit <woodenrabbit@qq.com>
2019-08-25 22:48:54 +0800

    version 1.0

--- app.js
+++ app.js
@@ -2,18 +2,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require('path');
+//post请求无法获取传递过来的参数
+const bodyParser = require("body-parser");
 
 // 实例化一个express对象
 let app = express();
 //使用静态资源库
 app.use("/public", express.static(path.join(__dirname, "/public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 //配置模板引擎
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // 给app绑定路由，所有通过"/"的url都将通过以下方法
-app.get("/", require('./routers/main.js'));
+app.get("/", require('./routers/index.js'));
+app.post("/content", require('./routers/main.js'));
 
 // 监听8080端口
 mongoose.connect("mongodb://localhost:27017/blog_db",(err)=>{
--- routers/main.js
+++ routers/main.js
@@ -2,19 +2,65 @@ const express = require('express');
 const router = express.Router();
 const contentModel = require("../models/content")
 
-// router.get('/',(req,res)=>{
-//     res.render("index");
+// router.get("/content", (req, res, next)=>{
+//     contentModel.find({},(err,all)=>{
+//         if(!err){
+//             res.json(all);
+//         }
+//         else{
+//             throw err;
+//         }
+//     });
 // });
 
-router.get("/", (req, res, next)=>{
-    contentModel.find({},(err,all)=>{
-        if(!err){
-            res.json(all);
-        }
-        else{
-            throw err;
-        }
-    });
-})
+// 内容添加的保存
+router.post("/content", (req, res, next) => {
+    let title = req.body.title;
+    let category = req.body.category;
+    let description = req.body.description;
+    let content = req.body.content;
+    // 后端进行简单的验证
+    if (title === "") {
+        // 如果标题为空，渲染错误页面
+        res.render("admin/error", {
+            url: null,
+            userInfo: req.userInfo,
+            message: "标题不能为空"
+        });
+        return;
+    } else if (description === "") {
+        // 如果简介为空，渲染错误页面
+        res.render("admin/error", {
+            url: null,
+            userInfo: req.userInfo,
+            message: "简介不能为空"
+        });
+        return;
+    } else if (content === "") {
+        // 如果正文为空，渲染错误页面
+        res.render("admin/error", {
+            url: null,
+            userInfo: req.userInfo,
+            message: "正文不能为空"
+        });
+        return;
+    } else {
+        // 一切正常，存入数据库
+        contentModel.create({
+            title: title,
+            category: category,
+            author: "woodenrabbit",
+            description: description,
+            content: content
+        }, (err) => {
+            if (!err) {
+                // 保存成功
+                res.send("提交成功！");
+            } else {
+                throw err;
+            }
+        });
+    }
+});
 
 module.exports = router;
